1d251428725008c75924801d992e35fea264d118
PatriotRossii <patriotrossii2019@mail.ru>
2023-03-18 12:23:02 +0300

    Setup tables from baseball, american_football and football

--- postgresql/schemas/db_1.sql
+++ postgresql/schemas/db_1.sql
@@ -1,3 +1,22 @@
+DROP SCHEMA IF EXISTS processor_schema CASCADE;
+
+CREATE SCHEMA IF NOT EXISTS processor_schema;
+
+CREATE TABLE IF NOT EXISTS processor_schema.baseball (
+    id SERIAL UNIQUE,
+    coefficient DOUBLE PRECISION
+);
+
+CREATE TABLE IF NOT EXISTS processor_schema.american_football (
+    id SERIAL UNIQUE,
+    coefficient DOUBLE PRECISION
+);
+
+CREATE TABLE IF NOT EXISTS processor_schema.football (
+    id SERIAL UNIQUE,
+    coefficient DOUBLE PRECISION
+);
+
 DROP SCHEMA IF EXISTS hello_schema CASCADE;
 
 CREATE SCHEMA IF NOT EXISTS hello_schema;
