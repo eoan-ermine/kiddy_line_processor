2e650bb944fb9c57364797cca1606999cad63d29
PatriotRossii <patriotrossii2019@mail.ru>
2023-03-18 15:43:47 +0300

    [grpc] Implement subscribe endpoint

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,10 @@
 #include <userver/utils/daemon_run.hpp>
 
 #include "hello/hello.hpp"
-#include "ready/ready.hpp"
 #include "hello/hello_client.hpp"
+#include "ready/ready.hpp"
+#include "subscribe/subscribe.hpp"
+
 
 int main(int argc, char* argv[]) {
   auto component_list =
@@ -25,6 +27,8 @@ int main(int argc, char* argv[]) {
   kiddy_line_processor::AppendHelloClient(component_list);
 
   kiddy_line_processor::AppendReady(component_list);
+
+  kiddy_line_processor::AppendSubscribe(component_list);
 
   return userver::utils::DaemonMain(argc, argv, component_list);
 }
---
+++ src/subscribe/subscribe.cpp
@@ -0,0 +1,38 @@
+#include "subscribe.hpp"
+
+#include <fmt/format.h>
+#include <chrono>
+
+#include <userver/clients/dns/component.hpp>
+
+namespace kiddy_line_processor {
+
+void Subscribe::SubscribeOnSportsLine(handlers::api::ProcessorServiceBase::SubscribeOnSportsLineCall& call) {
+  handlers::api::SubscribeInfo request;
+
+  while (call.Read(request)) {
+    std::vector<SportInfo> sports;
+    for(int i = 0, size = request.sports_size(); i != size; ++i) {
+      sports.push_back(SportInfo { request.sports(i), 0 });
+    }
+    int interval = request.interval();
+
+    std::cout << request.sports_size() << ' ' << request.interval() << '\n';
+
+    while (true) {
+      handlers::api::LinesInfo response;
+      RetrieveChanges(sports, *response.mutable_changes());
+      call.Write(response);
+
+      userver::engine::SleepFor(std::chrono::seconds(interval));
+    }
+  }
+
+  call.Finish();
+}
+
+void AppendSubscribe(userver::components::ComponentList& component_list) {
+  component_list.Append<Subscribe>();
+}
+
+}  // namespace kiddy_line_processor
